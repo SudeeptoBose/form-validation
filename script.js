cef7678505ca74925925a8e588fd3af38955a5c5
Sudeepto Bose <sudeeptobose23@gmail.com>
2021-09-05 23:44:56 +0600

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('form');
+const password1 = document.getElementById('password1');
+const password2 = document.getElementById('password2');
+const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
+
+let isValid = false;
+
+function validateForm() {
+    // Using Constraint API
+    isValid = form.checkValidity();
+    // Style main message for an error
+    message.textContent = 'Please fill out all fields.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+}
+
+function processFormData(e) {
+    e.preventDefault();
+    // Validate Form
+    validateForm();
+}
+
+
+// Event Listeners
+form.addEventListener('submit', processFormData);
